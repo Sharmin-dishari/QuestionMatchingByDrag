2e4907b565e088ab0900c1e5db844cd856e80165
Sharmin <sharminakter1820@gmail.com>
2023-02-26 17:10:09 +0600

    implement QnA matching using drag and drop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,193 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <div class="row" style="display: flex; flex-direction: row">
+    <div class="column right">
+      <div>
+        <div
+          v-for="(question, index) in questions"
+          :key="index"
+          v-bind:class="{ matched: matches[index] !== null }"
+          @drop="drop(index)"
+          @dragover.prevent
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+          <div class="row">
+            <div class="column left card cardColr">
+              {{ question.name }}
+            </div>
+            <div
+              class="column right card"
+              style="margin-left: 10px"
+              v-bind:draggable="true"
+              @dragstart="undoMatch(index)"
+              v-if="matches[index] !== null"
+            >
+              {{ matches[index].name }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="column left" style="margin-left: 30px"></div>
+    <div class="column center" style="">
+      <div
+        v-for="(answer, index) in answers"
+        :key="index"
+        v-bind:draggable="true"
+        class="card all-scroll"
+        @dragstart="dragStart(index)"
+      >
+        <div v-if="!matchedAnswers.includes(answer)">
+          {{ answer.name }}
+        </div>
+        <span v-else class="placeholder"></span>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  data: function () {
+    return {
+      questions: [
+        {
+          id: 1,
+          name: "Hi, Georgie.It's Simon",
+        },
+        {
+          id: 2,
+          name: "How about that new pub in Covent Garden - The Fallen Angel?",
+        },
+        {
+          id: 3,
+          name: "Fine. I was wondering if you were doing anything tomorrow evening.",
+        },
+        {
+          id: 4,
+          name: "Oh right. What are you going to see?",
+        },
+        {
+          id: 5,
+          name: "Right, well, I guess you're busy then",
+        },
+        {
+          id: 6,
+          name: "OK, well, do you fancy going for a drink afterwards?",
+        },
+      ],
+      answers: [
+        {
+          id: 1,
+          name: "Oh, hi Simon how are you",
+        },
+        {
+          id: 2,
+          name: "Tomorrow, Er, let me think, oh yes I'm goint an art exhibition after work",
+        },
+        {
+          id: 3,
+          name: "Fine. I was wondering if you were doing anything tomorrow evening.",
+        },
+        {
+          id: 4,
+          name: "Oh right. What are you going to see?",
+        },
+        {
+          id: 5,
+          name: "Right, well, I guess you're busy then",
+        },
+        {
+          id: 6,
+          name: "OK, well, do you fancy going for a drink afterwards?",
+        },
+      ],
+      matches: [null, null, null, null, null, null],
+      matchedAnswers: [],
+    };
+  },
+  methods: {
+    dragStart(index) {
+      this.matchedIndex = index;
+    },
+    dropQuestion(index) {
+      const questionIndex = event.dataTransfer.getData("text/plain");
+      const question = this.matchedAnswers.splice(questionIndex, 1)[0];
+      this.matchedAnswers.splice(index, 0, question);
+    },
+    drop(index) {
+      if (this.matches[index] !== null) {
+        // If the question already has a match, remove it
+        const matchedAnswer = this.matches[index];
+        const matchedAnswerIndex = this.matchedAnswers.indexOf(matchedAnswer);
+        this.matchedAnswers.splice(matchedAnswerIndex, 1);
+      }
+
+      // Set the new match and update the matchedAnswers list
+      this.matches.splice(index, 1, this.answers[this.matchedIndex]);
+      this.matchedAnswers.push(this.answers[this.matchedIndex]);
+      this.matchedIndex = null;
+    },
+    undoMatch(index) {
+      const matchedAnswer = this.matches[index];
+      const matchedAnswerIndex = this.matchedAnswers.indexOf(matchedAnswer);
+      this.matches.splice(index, 1, null);
+      this.matchedAnswers.splice(matchedAnswerIndex, 1);
+    },
   },
 };
 </script>
+<style>
+.column {
+  float: left;
+  padding: 10px;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+.left {
+  width: 50%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.all-scroll {
+  cursor: all-scroll;
 }
-li {
+.right {
+  width: 40%;
+  margin-left: 100px;
+}
+.center {
+  width: 20%;
+}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.card {
+  padding: 45px;
+  border: 1px solid black;
+  margin-bottom: 25px;
+  border-radius: 15px;
+  max-width: 230px;
+  height: 50px;
+}
+.cardColr {
+  background-color: #eef7fc;
+}
+.container {
+  padding: 2px 16px;
+}
+.placeholder {
   display: inline-block;
-  margin: 0 10px;
+  border: 1px solid #ccc;
+  margin-left: 5px;
 }
-a {
-  color: #42b983;
+@media only screen and (width: 400px) {
+  .left {
+    width: 60%;
+  }
+  .right {
+    width: 10%;
+    margin-left: 100px;
+  }
+  .center {
+    width: 30%;
+  }
 }
 </style>
